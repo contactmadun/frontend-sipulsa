4fbc1dc871fa4cf5b1bb3c335660af7ffae1c109
madun <contact.madun@gmail.com>
2023-04-03 12:17:17 -0700

    Create Login and Register Pages

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,8 +1,11 @@
 <template>
     <div class="text-start">
-        <h2 class="mt-5 fw-bold">Selamat datang kembali!</h2>
+        <h2 class="mt-5 fw-bold">Halo ini halaman register</h2>
         <p>Cari cuan? mulai dari sini.</p>
         <form class="mt-5">
+            <div class="mb-3">
+                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Nama Pengguna">
+            </div>
             <div class="mb-3">
                 <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Alamat Email">
             </div>
@@ -10,17 +13,16 @@
                 <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Kata Sandi">
             </div>
             <div class="d-grid gap-2 mt-5">
-                <button type="submit" class="btn btn-primary">Masuk</button>
+                <button type="submit" class="btn btn-primary">Daftar</button>
             </div>
-            <p class="text-center mt-2">Belum punya akun?<span><router-link to="/register">Daftar Disini</router-link></span></p>
+            <p class="text-center mt-2">Sudah punya akun?<span><router-link to="/">Masuk Disini</router-link></span></p>
         </form>
-        <p class="text-center fixed-bottom">Beta Version</p>
     </div>
 </template>
 
 <script>
 export default {
-    name: "LoginView",
+    name: "RegisterView",
 }
 </script>
 
