daad1a444dfb62d4d8cac00c6031c53193bd62b1
madun <contact.madun@gmail.com>
2023-04-03 09:35:51 -0700

    Connection Vue Projcet to Bootstrap and CSS

---
+++ src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>Senang Bertemu Denganmu Kembali Sahabat</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "LoginView",
+}
+</script>
+
+<style>
+
+</style>
